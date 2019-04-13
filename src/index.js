ca8fe452100997abfbbd4ce5aeb3bd77c80242d0
xxlxep <xxlxep@gmail.com>
2019-04-13 23:43:27 +0300

    Action Creators Redux Refactoring

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
-import reducer from './store/reducer';
+import burgerBuilder from './store/reducers/burgerBuilder';
 
-const store = createStore(reducer);
+const store = createStore(burgerBuilder, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 const app = (
     <Provider store={store}>
