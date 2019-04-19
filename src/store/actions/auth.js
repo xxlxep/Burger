b73e327eeb453bb368b2e3f505af409049971906
xxlxep <xxlxep@gmail.com>
2019-04-19 17:35:53 +0300

    save UserToken & Id in store, handling input errors, adding a spinner

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -8,10 +8,11 @@ export const authStart = () => {
     };
 };
 
-export const authSuccess = (authData) => {
+export const authSuccess = (token, userId) => {
     return {
         type: actionTypes.AUTH_SUCCESS,
-        authData: authData
+        idToken: token,
+        userId: userId
     };
 };
 
@@ -37,11 +38,11 @@ export const auth = ( email, password, isSignUp) => {
         axios.post( url, authData)
             .then(response => {
                 console.log(response);
-                dispatch(authSuccess(response.data));
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
             })
             .catch(err => {
-                console.log(err.response)
-                dispatch(authFail());
+
+                dispatch(authFail(err.response.data.error));
 
             });
     };
