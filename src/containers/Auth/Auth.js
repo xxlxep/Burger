7b490e9b24f7cde6a5b50b0e38cf8c5ea480a7be
xxlxep <xxlxep@gmail.com>
2019-04-19 13:08:44 +0300

    Sign Up request with firebase

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import classes from './Auth.css';
+import * as actions from '../../store/actions/index';
 
 class Auth extends Component {
 
@@ -83,6 +85,11 @@ class Auth extends Component {
         this.setState({controls: updatedControls});
     }
 
+    submitHandler = (event) => {
+        event.preventDefault();
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+    }
+
     render() {
         const formElementsArray = [];
         for (let key in this.state.controls) {
@@ -108,7 +115,7 @@ class Auth extends Component {
 
         return (
             <div className={classes.Auth}> 
-                <form>
+                <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType="Success" >SUBMIT</Button>
 
@@ -118,4 +125,10 @@ class Auth extends Component {
     }
 }
 
-export default Auth;
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: (email, password) => dispatch(actions.auth(email, password))
+    };
+};
+
+export default connect(null, mapDispatchToProps)(Auth);
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -8,3 +8,7 @@ export {purchaseBurger,
         purchaseInit,
         fetchOrders
  } from './order';
+
+export {
+    auth
+} from './auth';
