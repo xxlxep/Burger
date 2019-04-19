5d362819271bf351f7b98e45f8b2782243a139f9
xxlxep <xxlxep@gmail.com>
2019-04-19 14:37:32 +0300

    Sign In Sign Up added

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -22,15 +22,19 @@ export const authFail = (error) => {
     };
 };
 
-export const auth = ( email, password) => {
+export const auth = ( email, password, isSignUp) => {
     return dispatch => {
         dispatch(authStart());
         const authData = {
             email: email,
             password: password,
             returnSecureToken: true
+        };
+        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyC63toMzugbJZ-ImMadVsrOzHpAeBS9AJc';
+        if (!isSignUp) {
+            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyC63toMzugbJZ-ImMadVsrOzHpAeBS9AJc'
         }
-        axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyC63toMzugbJZ-ImMadVsrOzHpAeBS9AJc', authData)
+        axios.post( url, authData)
             .then(response => {
                 console.log(response);
                 dispatch(authSuccess(response.data));
