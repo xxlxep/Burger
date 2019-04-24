424bf8bbcc00db85c29ea53dcbfbeca8dddd3434
xxlxep <xxlxep@gmail.com>
2019-04-24 13:37:52 +0300

    Adding LazyLoading Orders, checkot, auth

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import classes from './Logo.css';
 
 const Logo = (props) => (
     <div className={classes.Logo} style={{height: props.height}}>
-        <img src={burgerLogo} />
+        <img src={burgerLogo} alt="logo" />
     </div>
 );
 
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -11,9 +11,6 @@ class Modal extends Component {
        
     }
 
-    componentWillUpdate() {
-        console.log('[Modal] will update');
-    }
 
     render () {
         return (
